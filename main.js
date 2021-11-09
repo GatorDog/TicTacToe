261ec86350c24682ebd0a8825d4cbbcfe3faaa46
Tim Dailey <tdailey@lcs.com>
2021-11-09 09:47:36 -0500

    Working game pre-polish

--- main.js
+++ main.js
@@ -2,53 +2,66 @@ const playerFactory = (name, marker) => {
     return {name, marker};
 };
 
-const playerOne = playerFactory("Tim", "X");
-const playerTwo = playerFactory("Dani", "O");
+const playerOne = playerFactory("playerOne", "X");
+const playerTwo = playerFactory("playerTwo", "O");
 
 
 const gameBoardFlowModule = (() => {
+
+    let gameOver = false;
+
+    let tileMarker = (id) => {
+        gameBoard.tiles[id].marker = gameFlow.activePlayer.marker;
+    }
     let gameBoard = {
         tiles: 
         [
             {
                 position: 0,
-                marker: "none"
+                marker: "none0"
             },
             {
                 position: 1,
-                marker: "none"
+                marker: "none1"
             },
             {
                 position: 2,
-                marker: "none"
+                marker: "none2"
             },
             {
                 position: 3,
-                marker: "none"
+                marker: "none3"
             },
             {
                 position: 4,
-                marker: "none"
+                marker: "none4"
             },
             {
                 position: 5,
-                marker: "none"
+                marker: "none5"
             },
             {
                 position: 6,
-                marker: "none"
+                marker: "none6"
             },
             {
                 position: 7,
-                marker: "none"
+                marker: "none7"
             },
             {
                 position: 8,
-                marker: "none"
+                marker: "none8"
             }
         ]
     };
+    /* */
 
+    let tileChecker = (tileOne, tileTwo, tileThree) => {
+        if (gameBoard.tiles[tileOne].marker == gameBoard.tiles[tileTwo].marker && gameBoard.tiles[tileOne].marker == gameBoard.tiles[tileThree].marker) {
+            alert(`player ${gameFlow.activePlayer.name} has won!`)
+            gameBoardFlowModule.gameOver = true;
+        }
+    }
 
     let turnCounter = 1;
     let gameFlow = {
@@ -56,11 +69,12 @@ const gameBoardFlowModule = (() => {
         activePlayer: playerOne
     }
 
-    return{gameBoard, gameFlow};
+    return{gameBoard, gameFlow, gameOver, tileMarker, tileChecker};
 
 })();
 
 const displayController = (() => {
+
 
     let gameBoardContainer = document.getElementById("gameBoardContainer");
     let i = 0;
@@ -73,11 +87,28 @@ const displayController = (() => {
         cell.addEventListener("click", (() => {
             //Check to see if the clicked box has been marked already
 
-          if (cell.className == "gameBoardCell noSelect"){
+          if (cell.className == "gameBoardCell noSelect" && gameBoardFlowModule.gameOver == false){
               //Clicked box has not been marked yet, mark it with the current active players marker and add marked class
 
-            cell.innerHTML = gameBoardFlowModule.gameFlow.activePlayer.marker; 
-            cell.className = "gameBoardCell noSelect marked";
+            gameBoardFlowModule.gameFlow.turn++;
+            cell.innerHTML = gameBoardFlowModule.gameFlow.activePlayer.marker;
+            gameBoardFlowModule.tileMarker(cell.id);
+            cell.classList.add("marked");
+            
+            //check to see if any winning combos are present
+
+            gameBoardFlowModule.tileChecker(0, 1, 2);
+            gameBoardFlowModule.tileChecker(3, 4, 5);
+            gameBoardFlowModule.tileChecker(6, 7, 8);
+            gameBoardFlowModule.tileChecker(0, 3, 6);
+            gameBoardFlowModule.tileChecker(1, 4, 7);
+            gameBoardFlowModule.tileChecker(2, 5, 8);
+            gameBoardFlowModule.tileChecker(0, 4, 8);
+            gameBoardFlowModule.tileChecker(2, 4, 6);
+
+            if (gameBoardFlowModule.gameFlow.turn == 10 && gameBoardFlowModule.gameOver == false){
+                alert("Tie Game!");
+            }
 
             //Check to see what players turn it currently is and flip it
 
@@ -94,11 +125,40 @@ const displayController = (() => {
               console.log(cell.id)
           }
 
-
         }))
 
+        
         gameBoardContainer.appendChild(cell);
         i++;
     });
 
 })();
+
+const gameSetup = (() => {
+
+let playerOneInput = document.getElementById("playerOneName");
+let playerTwoInput = document.getElementById("playerTwoName");
+let submitButton = document.getElementById("submit");
+let resetBoard = document.getElementById("resetBoard");
+
+
+resetBoard.addEventListener("click", (() => {
+    for(let i = 0; i < 9; i++) {
+        let resetTile = document.getElementById(i)
+
+        resetTile.className = "gameBoardCell noSelect";
+        resetTile.innerHTML = "";
+        gameBoardFlowModule.gameBoard.tiles[i].marker = `none${i}`
+
+    }
+    gameBoardFlowModule.gameOver = false;
+    gameBoardFlowModule.gameFlow.turn = 1;
+    gameBoardFlowModule.gameFlow.activePlayer = playerOne;
+}))
+
+submitButton.addEventListener("click", (() => {
+    playerOne.name = playerOneInput.value;
+    playerTwo.name = playerTwoInput.value;
+}))
+
+})()
